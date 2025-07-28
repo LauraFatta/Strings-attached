300c9026f2b65007c1d32c9c29da3cee325050e7
SAMI AHMED ABDULMAJEED JABARTI <samyjabarti@hotmail.com>
2025-07-28 04:02:56 +0300

    Fixed some merging problems and added Juman's final fixes

--- Assets/Juman/SimpleStepManager.cs
+++ Assets/Juman/SimpleStepManager.cs
@@ -3,26 +3,18 @@ using UnityEngine.UI;
 
 public class SimpleStepManager : MonoBehaviour
 {
-    public Button buttonStart;
     public GameObject imageClosedBag;
     public Button bagButton; // زر الشنطة داخل الصورة
     public GameObject puzzlePanel;
 
     void Start()
     {
-        // ربط الأحداث
-        buttonStart.onClick.AddListener(ShowBagImage);
+        // ربط الحدث
         bagButton.onClick.AddListener(ShowPuzzlePanel);
 
-        // إخفاء العناصر بعد الربط
-        imageClosedBag.SetActive(false);
-        puzzlePanel.SetActive(false);
-    }
-
-    void ShowBagImage()
-    {
-        buttonStart.gameObject.SetActive(false);
+        // إظهار صورة الشنطة فقط
         imageClosedBag.SetActive(true);
+        puzzlePanel.SetActive(false);
     }
 
     void ShowPuzzlePanel()
