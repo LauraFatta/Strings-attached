3e46f410ec8e1342729d47dccca85017ce496fa1
jumanAlfhmi <56445875+jumanAlfhmi@users.noreply.github.com>
2025-08-10 20:04:10 +0300

    edit case file not final
    ..

--- Assets/Nora/Scripts/CabinetActivator.cs
+++ Assets/Nora/Scripts/CabinetActivator.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
@@ -12,18 +11,18 @@ public class DrawerActivator : MonoBehaviour
     [SerializeField] public GameObject drawerCamera;
     [SerializeField] private GameObject closeButton;
     [SerializeField] private GameObject allButton;
+
     [SerializeField] private GameObject DlabFileButton;
+    [SerializeField] private GameObject DlabFileButton2; // زر ثاني
+    [SerializeField] private GameObject DlabFileButton3; // زر ثالث
+
     [SerializeField] private GameObject FileBackground;
     [SerializeField] private GameObject OpenFileButton;
     [SerializeField] private GameObject BookPanel;
-    // [SerializeField] private GameObject BackButton;
 
     [SerializeField] private GameObject SuspectsFileBook;
     [SerializeField] private GameObject SuspectsFileBook2;
     [SerializeField] private GameObject WitnessFileBook;
-    
-
-
 
     public void OpenDrawer()
     {
@@ -34,10 +33,9 @@ public class DrawerActivator : MonoBehaviour
         closeButton.SetActive(true);
         allButton.SetActive(false);
         DlabFileButton.SetActive(true);
-        // PnelFileBook.SetActive(true);
-
+        DlabFileButton2.SetActive(true);
+        DlabFileButton3.SetActive(true);
     }
-
 
     public void CloseDrawer()
     {
@@ -48,52 +46,54 @@ public class DrawerActivator : MonoBehaviour
         closeButton.SetActive(false);
         allButton.SetActive(true);
         DlabFileButton.SetActive(false);
+        DlabFileButton2.SetActive(false);
+        DlabFileButton3.SetActive(false);
         FileBackground.SetActive(false);
         OpenFileButton.SetActive(false);
         BookPanel.SetActive(false);
         SuspectsFileBook.SetActive(false);
         SuspectsFileBook2.SetActive(false);
         WitnessFileBook.SetActive(false);
-        
-
-        
-
     }
+
     public void Openfile()
     {
         FileBackground.SetActive(true);
         OpenFileButton.SetActive(true);
         DlabFileButton.SetActive(false);
+        DlabFileButton2.SetActive(false);
+        DlabFileButton3.SetActive(false);
     }
 
     public void openFileBook()
     {
         BookPanel.SetActive(true);
-        // BackButton.SetActive(true);
         DlabFileButton.SetActive(false);
+        DlabFileButton2.SetActive(false);
+        DlabFileButton3.SetActive(false);
     }
 
     public void nextWitnessFileBook()
     {
         SuspectsFileBook.SetActive(true);
         WitnessFileBook.SetActive(false);
-
     }
+
     public void nextSuspectsFileBook()
     {
         SuspectsFileBook2.SetActive(true);
         SuspectsFileBook.SetActive(false);
-
     }
+
     public void previousSuspectsFileBook()
     {
         WitnessFileBook.SetActive(true);
         SuspectsFileBook.SetActive(false);
     }
+
     public void previousSuspectsFileBook2()
     {
         SuspectsFileBook.SetActive(true);
         SuspectsFileBook2.SetActive(false);
     }
-
 }
